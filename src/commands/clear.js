810de708a9a53bed8021d3f29e9c1911ef1ac945
Andrei Ticarat <75678080+Kerolly@users.noreply.github.com>
2021-08-23 16:28:52 +0300

    Added prefix change
    Added prefix change but isn't save in a database

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -2,17 +2,27 @@ module.exports = {
   name: "clear",
   description: "Clear messages",
   async execute(message, args) {
-
     // Safety check
-    if(!args[0]) return message.reply('You need to provide a number of arguments');
-    if(isNaN(args[0])) return message.reply('You must provied a real number')
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+    return message.reply(`You haven't permission to run this command`);
     
-    if(args[0] <= 0) return message.reply('You must provide a number greater than zero')
-    if(args[0] > 100) return message.reply('You must provide a number smaller than or equal to 100')
+    if (!args[0])
+      return message.reply("You need to provide a number of arguments");
+    if (isNaN(args[0])) return message.reply("You must provied a real number");
+   
+
+    if (args[0] <= 0)
+      return message.reply("You must provide a number greater than zero");
+    if (args[0] > 100)
+      return message.reply(
+        "You must provide a number smaller than or equal to 100"
+      );
 
     //Delete Command
     await message.channel.messages
       .fetch({ limit: args[0] })
-      .then((messages) => {message.channel.bulkDelete(messages)});
+      .then((messages) => {
+        message.channel.bulkDelete(messages);
+      });
   },
 };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,41 @@ require("dotenv").config();
 
 const memberCounter = require("./counters/memberCounter"); // member counter
 
-const prefix = process.env.PREFIX;
+let prefix = "!";
+
+// const validPermissions = [
+//   "CREATE_INSTANT_INVITE",
+//   "KICK_MEMBERS",
+//   "BAN_MEMBERS",
+//   "ADMINISTRATOR",
+//   "MANAGE_CHANNELS",
+//   "MANAGE_GUILD",
+//   "ADD_REACTIONS",
+//   "VIEW_AUDIT_LOG",
+//   "PRIORITY_SPEAKER",
+//   "STREAM",
+//   "VIEW_CHANNEL",
+//   "SEND_MESSAGES",
+//   "SEND_TTS_MESSAGES",
+//   "MANAGE_MESSAGES",
+//   "EMBED_LINKS",
+//   "ATTACH_FILES",
+//   "READ_MESSAGE_HISTORY",
+//   "MENTION_EVERYONE",
+//   "USE_EXTERNAL_EMOJIS",
+//   "VIEW_GUILD_INSIGHTS",
+//   "CONNECT",
+//   "SPEAK",
+//   "MUTE_MEMBERS",
+//   "DEAFEN_MEMBERS",
+//   "MOVE_MEMBERS",
+//   "USE_VAD",
+//   "CHANGE_NICKNAME",
+//   "MANAGE_NICKNAMES",
+//   "MANAGE_ROLES",
+//   "MANAGE_WEBHOOKS",
+//   "MANAGE_EMOJIS",
+// ];
 
 client.on("ready", () => {
   console.log(`Logged is as ${client.user.tag}`);
@@ -104,6 +138,15 @@ client.on("message", (message) => {
     case "set-volume":
     case "v":
       client.commands.get("volume").execute(client, message, args);
+      break;
+
+    case "prefix":
+      if (!args[0])
+        return message.reply(
+          "Please enter any character that you want to be the prefix"
+        );
+
+      prefix = args[0];
       break;
   }
 
